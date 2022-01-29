a84e7ad4b813077d8a9f21285aaf1142eefc85ad
sorayeon <jihwan77@gmail.com>
2022-01-29 20:59:06 +0900

    이미지 에셋 세팅
    감정 이미지들을 프로젝트에서 불러와 사용할 수 있는 환경 세팅

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+        <img src={process.env.PUBLIC_URL + '/assets/emotion1.png'} alt="행복" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion2.png'} alt="좋음" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion3.png'} alt="보통" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion4.png'} alt="우울" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion5.png'} alt="나쁨" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
