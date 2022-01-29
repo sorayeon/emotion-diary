7a453f551f5f7e6bb3f92263185daac771cbbc73
sorayeon <jihwan77@gmail.com>
2022-01-29 16:15:47 +0900

    React Router 응용 #1
    1. Path Variable : useParams
    - const {id} = useParams();
     : <Route path="/diary/:id"
    
    2. Query String : useSearchParams
    - const [searchParams, setSearchParams] = useSearchParams();
     : const id = searchParams.get('id');
     : setSearchParams({ who: 'sorayeon' })
      쿼리 스트링 변경
    
    3. Page Moving
    - const navigate = useNavigate();
     : 로그인 하지 않은 요청 로그인 페이지로 이동
       navigate('/login');
     : history.back() -> navigate(-1);

--- src/components/RouteTest.js
+++ src/components/RouteTest.js
@@ -5,11 +5,11 @@ const RouteTest = () => {
     <>
       <Link to={'/'}>HOME</Link>
       <br />
-      <Link to={'/diary'}>DIARY</Link>
+      <Link to={'/diary/1'}>DIARY</Link>
       <br />
       <Link to={'/new'}>NEW</Link>
       <br />
-      <Link to={'/edit'}>EDIT</Link>
+      <Link to={'/edit?id=10&mode=dark'}>EDIT</Link>
     </>
   );
 };
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 const Diary = () => {
+  const { id } = useParams();
+  console.log(`id : ${id}`);
   return (
     <div>
       <h1>Diary</h1>
