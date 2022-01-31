547cf5c93e204230622c4bcef525783c6f1dbfc7
sorayeon <jihwan77@gmail.com>
2022-02-01 04:18:48 +0900

    Diary 구현
    일기 상세기능 구현
    
    Diary Page
    : Path Variable :id 로 넘어온 데이터 가져오기
    : 일기 목록을 useContext() 에서 조회
    : 일기 목록에서 id값 으로 수정할 일기를 find()
    - data state에 find한 데이터 set
    - 없으면 Home로 이동
    : Header - 뒤로가기, 수정하기 버튼
    : 감정, 일기내용 화면 구현
    
    프로젝트 전반에 사용하는 공통함수 utils 폴더에 작성
    date.js
    - 날자를 YYYY. MM. DD 형태로 반환
    emotion.js
    - 감정ID, IMAGE URL, 감정DESC 정보를 가진 배열

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,14 +1,76 @@
-import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import MyButton from '../components/MyButton';
+import MyHeader from '../components/MyHeader';
+import { getStringDate } from '../utils/date';
+import { emotionList } from '../utils/emotion';
 
 const Diary = () => {
   const { id } = useParams();
-  console.log(`id : ${id}`);
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 일기 상세 페이지 입니다.</p>
-    </div>
-  );
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const targetDiary = diaryList.find(
+      (it) => parseInt(it.id, 10) === parseInt(id, 10),
+    );
+
+    if (targetDiary) {
+      setData(targetDiary);
+    } else {
+      alert('없는 일기입니다.');
+      navigate('/', { replace: true });
+    }
+  }, [diaryList, id]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id, 10) === parseInt(data.id, 10),
+    );
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <MyButton text="&larr; 뒤로가기" onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text="수정하기"
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div
+              className={[
+                'diary_img_wrapper',
+                `diary_img_wrapper_${data.emotion}`,
+              ].join(' ')}
+            >
+              <img
+                src={curEmotionData.emotion_img}
+                alt={curEmotionData.emotion_desc}
+              />
+              <div className="emotion_desc">{curEmotionData.emotion_desc}</div>
+            </div>
+          </section>
+          <section>
+            <h4>오늘의 일기</h4>
+            <div className="diary_content_wrapper">
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
+      </div>
+    );
+  }
 };
 
 export default Diary;
